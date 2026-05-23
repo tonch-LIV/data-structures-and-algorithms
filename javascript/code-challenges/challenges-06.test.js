f2cd8a8ba8016b56e221a32f2810efdb297a5ac0
Antonio Garcia Pardo <garcia54.a@gmail.com>
2026-05-23 13:53:40 -0700

    challenge 6; passed problem 3

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -46,6 +46,9 @@ Write a function named addPurchases that, given an array of objects as input, us
 
 const addPurchases = (arr) => {
   // Solution code here...
+  return arr.reduce((accumulator, current) => {
+    return accumulator + current.purchasePrice
+  }, 0)
 };
 
 /* ------------------------------------------------------------------------------------------------
