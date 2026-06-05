c34a29c969cb0d90bf1d146f611ff701043ed0b5
Antonio Garcia Pardo <garcia54.a@gmail.com>
2026-06-04 22:38:57 -0700

    challenge08; passed problem 3

--- javascript/code-challenges/challenges-08.test.js
+++ javascript/code-challenges/challenges-08.test.js
@@ -51,6 +51,7 @@ Write a function named joinArray that takes an array and joins all of the elemen
 
 const joinArray = (arr) => {
   // Solution code here...
+  return arr.join(' ');
 };
 
 /* ------------------------------------------------------------------------------------------------
