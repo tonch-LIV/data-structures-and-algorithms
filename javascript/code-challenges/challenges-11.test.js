66549d5cd9ee850fc0c7c3d15e95887c737f5085
Antonio Garcia Pardo <garcia54.a@gmail.com>
2026-06-29 20:01:32 -0700

    challenge11; pass problem 4

--- javascript/code-challenges/challenges-11.test.js
+++ javascript/code-challenges/challenges-11.test.js
@@ -59,6 +59,12 @@ For example, (123) 456-7890 returns 1234567890
 
 const standardizePhoneNumbers = (arr) => {
   // Solution code here...
+  return arr.map(phone => 
+    phone.replace("(", "")
+    .replace(")", "")
+    .replace(" ", "")
+    .replace("-", "")
+  );
 };
 
 /* ------------------------------------------------------------------------------------------------
