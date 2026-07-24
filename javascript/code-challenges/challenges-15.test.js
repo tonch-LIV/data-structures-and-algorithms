1d2cf08753d22ea2630c02270cee703d5581c28d
Antonio Garcia Pardo <garcia54.a@gmail.com>
2026-07-24 13:45:08 -0700

    challenge15; pass problem-04

--- javascript/code-challenges/challenges-15.test.js
+++ javascript/code-challenges/challenges-15.test.js
@@ -135,6 +135,12 @@ This data could be sorted by name or price.
 
 const sortBy = (property, arr) => {
   // Solution code here...
+  return arr.sort((a, b) => {
+    if (property === "price") {
+      return a.price - b.price;
+    }
+    return a.name.localeCompare(b.name);
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
