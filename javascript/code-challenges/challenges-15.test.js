68103b6214bbd4509075a6586489e41fa388980a
Antonio Garcia Pardo <garcia54.a@gmail.com>
2026-07-24 14:15:57 -0700

    challenge15; pass problem-04

--- javascript/code-challenges/challenges-15.test.js
+++ javascript/code-challenges/challenges-15.test.js
@@ -157,6 +157,7 @@ https:/missingslash.org returns false because the URL is malformed
 ------------------------------------------------------------------------------------------------ */
 const isSecure = (url) => {
   // Solution code here...
+  return /^https:\/\/.+$/.test(url);
 };
 
 /* ------------------------------------------------------------------------------------------------
